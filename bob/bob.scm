c8e748e51406617bc4cc9ca8c1d566ca25c85ba1
Jason Lewis <jason@decomplecting.org>
2015-01-30 00:15:00 -0500

    Add 'bob' exercise

---
+++ bob/bob.scm
@@ -0,0 +1,3 @@
+(define-module (bob)
+  #:export (response-for))
+
---
+++ bob/example.scm
@@ -0,0 +1,26 @@
+(define-module (bob)
+  #:export (response-for))
+
+(define shouting?
+  (lambda (phrase)
+    (and (string-any char-alphabetic? phrase)
+         (string-every char-upper-case?
+                   (string-filter char-alphabetic? phrase)))))
+
+(define question?
+  (lambda (phrase)
+    (char=? #\?
+            (string-ref phrase
+                        (- (string-length phrase) 1)))))
+
+(define silent?
+  (lambda (phrase)
+    (or (string-null? phrase)
+        (string-every char-whitespace? phrase))))
+
+(define response-for
+  (lambda (phrase)
+    (cond ((silent? phrase) "Fine. Be that way!")
+          ((shouting? phrase) "Whoa, chill out!")
+          ((question? phrase) "Sure.")
+          (else "Whatever."))))
