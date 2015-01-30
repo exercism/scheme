726680d25f507adb89535e5cf2bf50a1b1df6814
Jason Lewis <jason@decomplecting.org>
2015-01-30 02:41:48 -0500

    Add difference-of-squares

---
+++ difference-of-squares/difference-of-squares-test.scm
@@ -0,0 +1,42 @@
+;; Load SRFI-64 lightweight testing specification
+(use-modules (srfi srfi-64))
+
+;; Require module
+(add-to-load-path (dirname (current-filename)))
+(use-modules (squares))
+
+(test-begin "difference-of-squares")
+
+(test-eqv "square-of-sums-to-5"
+          225
+          (square-of-sums 5))
+(test-eqv "sum-of-squares-to-5"
+          55
+          (sum-of-squares 5))
+(test-eqv "difference of-sums-to-5"
+          170
+          (difference 5))
+
+(test-eqv "square-of-sums-to-10"
+          3025
+          (square-of-sums 10))
+(test-eqv "sum-of-squares-to-10"
+          385
+          (sum-of-squares 10))
+(test-eqv "difference of-sums-to-10"
+          2640
+          (difference 10))
+
+(test-eqv "square-of-sums-to-100"
+          25502500
+          (square-of-sums 100))
+(test-eqv "sum-of-squares-to-100"
+          338350
+          (sum-of-squares 100))
+(test-eqv "difference of-sums-to-100"
+          25164150
+          (difference 100))
+
+
+
+(test-end "difference-of-squares")
