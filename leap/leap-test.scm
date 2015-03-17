afc7b51a17f0dc907f5f672bd0d96c4a7fc1a346
Jason Lewis <jason@decomplecting.org>
2015-03-17 16:24:04 -0400

    Add comment explaining the redef and how to get logs if you want them

--- leap/leap-test.scm
+++ leap/leap-test.scm
@@ -1,5 +1,7 @@
 ;; Load SRFI-64 lightweight testing specification
 (use-modules (srfi srfi-64))
+
+;; Suppress log file output. To write logs, comment out the following line:
 (module-define! (resolve-module '(srfi srfi-64)) 'test-log-to-file #f)
 
 ;; Require module
