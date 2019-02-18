2871cbdd902391f25688b51638eb13cd86ddb966
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-18 16:02:18 -0500

    run unit tests in Travis builds

---
+++ bin/run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+failed=0
+ROOT="$(pwd)"
+for exercise_directory in $(find ./exercises/* -type d); do
+    tmpdir=$(mktemp -d)
+    test_file="$(find "$exercise_directory" -type f -name '*-test.scm')"
+    solution_file="$exercise_directory/example.scm"
+    cp "$test_file" "$tmpdir"
+    cp "$solution_file" "$tmpdir/$(basename "${test_file//-test}")"
+    cd "$tmpdir" && guile "$test_file"
+    ret="$?"
+    cd "$ROOT"
+    rm -rf "$tmpdir"
+    if [ -n "$ret" ]; then
+        failed="$(( failed + 1 ))"
+    fi
+done
+
+echo "$failed failed exercises."
+exit "$failed"
