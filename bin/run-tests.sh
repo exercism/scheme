1ab090c14d31d4cfbcb5a00aaf1f127134f94bfc
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-19 08:24:06 -0500

    fix solution filename matching

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -5,9 +5,10 @@ ROOT="$(pwd)"
 for exercise_directory in $(find ./exercises/* -type d); do
     tmpdir=$(mktemp -d)
     test_file="$(find "$exercise_directory" -type f -name '*-test.scm')"
-    solution_file="$exercise_directory/example.scm"
+    example_file="$exercise_directory/example.scm"
+    solution_file="$(find "$exercise_directory" -type f -name '*.scm' | grep -vE 'example|test')"
     cp "$test_file" "$tmpdir"
-    cp "$solution_file" "$tmpdir/$(basename "${test_file//-test}")"
+    cp "$example_file" "$tmpdir/$(basename "$solution_file")"
     cd "$tmpdir" && guile "$(basename $test_file)"
     ret="$?"
     cd "$ROOT"
