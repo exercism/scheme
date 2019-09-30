63deb62f78eae6ec79cdc875281c43aa90984edc
jitwit <jrn@bluefarm.ca>
2019-09-30 15:11:31 -0400

    letrec instead of defines

--- exercises/knapsack/example.scm
+++ exercises/knapsack/example.scm
@@ -2,45 +2,45 @@
 
 (load "test.scm")
 
-(define (greedy-order weight-value-pairs)
+(define (knapsack capacity weights values)
+  (branch&bound capacity (order-by-density (map cons weights values))))
+
+(define (branch&bound capacity items)
+  (letrec ((best 0)
+	   (relaxed-estimate (lambda (capacity value item)
+			       (+ value (* (cdr item)
+					   capacity
+					   (/ (car item))))))
+	   (bound (lambda (capacity value items)
+		    (do ((items items (cdr items)))
+			((or (null? items)
+			     (< capacity (caar items)))
+			 (if (null? items)
+			     value
+			     (relaxed-estimate capacity value (car items))))
+		      (set! capacity (- capacity (caar items)))
+		      (set! value (+ value (cdar items))))))
+	   (branch (lambda (capacity value items)
+		     (when (< best value)
+		       (set! best value))
+		     (unless (null? items)
+		       (when (<= best (bound capacity value items))
+			 (when (<= (caar items) capacity)
+			   (branch (- capacity (caar items))
+				   (+ value (cdar items))
+				   (cdr items)))
+			 (branch capacity
+				 value
+				 (cdr items)))))))
+    (branch capacity 0 items)
+    best))
+
+(define (order-by-density items)
   (list-sort (lambda (w1.v1 w2.v2)
 	       (> (/ (cdr w1.v1) (car w1.v1))
 		  (/ (cdr w2.v2) (car w2.v2))))
-	     weight-value-pairs))
+	     items))
 
-(define (knapsack capacity weights values)
-  (branch&bound capacity
-		(greedy-order (map cons weights values))))
-
-
-(define (branch&bound capacity weights-values)
-  (define best 0)
-  (define (bound capacity value weights-values)
-    (do ((weights-values weights-values (cdr weights-values)))
-	((or (null? weights-values)
-	     (< capacity (caar weights-values)))
-	 (if (null? weights-values)
-	     value
-	     (+ value (* (cdar weights-values)
-			 capacity
-			 (/ (caar weights-values))))))
-      (set! capacity (- capacity (caar weights-values)))
-      (set! value (+ value (cdar weights-values)))))
-  (let branch ((capacity capacity)
-	       (total 0)
-	       (weights-values weights-values))
-    (when (< best total)
-      (set! best total))
-    (unless (null? weights-values)
-      (when (<= best (bound capacity total weights-values))
-	(when (<= (caar weights-values) capacity)
-	  (branch (- capacity (caar weights-values))
-		  (+ total (cdar weights-values))
-		  (cdr weights-values)))
-	(branch capacity
-		total
-		(cdr weights-values)))))
-  best)
 
 
 
