aadd8093fe3e54fa278973a9ba0b5d67364d5b2e
jitwit <jrn@bluefarm.ca>
2019-10-03 02:28:29 -0400

    perfect numbers

---
+++ code/exercises/perfect-numbers/example.scm
@@ -0,0 +1,45 @@
+(import (rnrs (6)))
+
+(load "test.scm")
+
+(define (classify n)
+  (when (or (< n 1) (not (integer? n)))
+    (error 'classify "not a natural number" n))
+  (let ((D-n (- (apply + (divisors n)) n)))
+    (cond ((< D-n n) 'deficient)
+          ((> D-n n) 'abundant)
+          (else 'perfect))))
+
+(define (divisors n)
+  (fold-left (lambda (xs y)
+               (nub-merge xs (map (lambda (x)
+                                    (* x y))
+                                  xs)))
+             '(1)
+             (factorize n)))
+
+(define (factorize n)
+  (if (even? n)
+      (cons 2 (factorize (/ n 2)))
+      (let loop ((d 3) (n n))
+        (cond ((< n (square d))
+               (if (= n 1) '() (list n)))
+              ((divides? d n)
+               (cons d (loop d (/ n d))))
+              (else
+               (loop (+ d 2) n))))))
+
+(define (nub-merge xs ys)
+  (if (null? xs)
+      ys
+      (cond ((< (car xs) (car ys))
+             (cons (car xs) (nub-merge (cdr xs) ys)))
+            ((> (car xs) (car ys))
+             (cons (car ys) (nub-merge (cdr ys) xs)))
+            (else (cons (car xs) (nub-merge (cdr xs) (cdr ys)))))))
+
+(define (square x)
+  (* x x))
+
+(define (divides? d n)
+  (zero? (modulo n d)))
---
+++ exercises/perfect-numbers/test.scm
@@ -0,0 +1,135 @@
+(define test-fields '(input output who))
+
+(define (test-run-solution solution input)
+  (if (procedure? solution) (apply solution input) solution))
+
+(define (test-success description success-predicate
+         procedure input output)
+  (call/cc
+    (lambda (k)
+      (with-exception-handler
+        (lambda (e)
+          (k `(fail
+                (description . ,description)
+                (input . ,input)
+                (output . ,output)
+                (who . ,procedure))))
+        (lambda ()
+          (let ([result (test-run-solution procedure input)])
+            (unless (success-predicate result output)
+              (error 'exercism-test
+                "test fails"
+                description
+                input
+                result
+                output)))
+          `(pass . ,description))))))
+
+(define (test-error description procedure input)
+  (call/cc
+    (lambda (k)
+      (with-exception-handler
+        (lambda (e) (k `(pass . ,description)))
+        (lambda ()
+          (test-run-solution procedure input)
+          `(fail
+             (description . ,description)
+             (input . ,input)
+             (output . error)
+             (who . ,procedure)))))))
+
+(define (run-test-suite tests . query)
+  (for-each
+    (lambda (field)
+      (unless (and (symbol? field) (memq field test-fields))
+        (error 'run-test-suite
+          (format "~a not in ~a" field test-fields))))
+    query)
+  (let-values ([(passes failures)
+                (partition
+                  (lambda (result) (eq? 'pass (car result)))
+                  (map (lambda (test) (test)) tests))])
+    (cond
+      [(null? failures) (format #t "~%Well done!~%~%") 'success]
+      [else
+       (format
+         #t
+         "~%Passed ~a/~a tests.~%~%The following test cases failed:~%~%"
+         (length passes)
+         (length tests))
+       (for-each
+         (lambda (failure)
+           (format
+             #t
+             "* ~a~%"
+             (cond
+               [(assoc 'description (cdr failure)) => cdr]
+               [else (cdr failure)]))
+           (for-each
+             (lambda (field)
+               (let ([info (assoc field (cdr failure))])
+                 (format #t "  - ~a: ~a~%" (car info) (cdr info))))
+             query))
+         failures)
+       (newline)
+       'failure])))
+
+(define (test . args)
+  (apply
+    run-test-suite
+    (list
+      (lambda ()
+        (test-success
+          "Smallest perfect number is classified correctly" eq?
+          classify '(6) 'perfect))
+      (lambda ()
+        (test-success
+          "Medium perfect number is classified correctly" eq? classify
+          '(28) 'perfect))
+      (lambda ()
+        (test-success "Large perfect number is classified correctly"
+          eq? classify '(33550336) 'perfect))
+      (lambda ()
+        (test-success
+          "Smallest abundant number is classified correctly" eq?
+          classify '(12) 'abundant))
+      (lambda ()
+        (test-success
+          "Medium abundant number is classified correctly" eq?
+          classify '(30) 'abundant))
+      (lambda ()
+        (test-success
+          "Large abundant number is classified correctly" eq? classify
+          '(33550335) 'abundant))
+      (lambda ()
+        (test-success
+          "Smallest prime deficient number is classified correctly"
+          eq? classify '(2) 'deficient))
+      (lambda ()
+        (test-success
+          "Smallest non-prime deficient number is classified correctly"
+          eq? classify '(4) 'deficient))
+      (lambda ()
+        (test-success
+          "Medium deficient number is classified correctly" eq?
+          classify '(32) 'deficient))
+      (lambda ()
+        (test-success
+          "Large deficient number is classified correctly" eq?
+          classify '(33550337) 'deficient))
+      (lambda ()
+        (test-success
+          "Edge case (no factors other than itself) is classified correctly"
+          eq? classify '(1) 'deficient))
+      (lambda ()
+        (test-error
+          "Zero is rejected (not a natural number)"
+          classify
+          '(0)))
+      (lambda ()
+        (test-error
+          "Negative integer is rejected (not a natural number)"
+          classify
+          '(-1))))
+    args))
+
