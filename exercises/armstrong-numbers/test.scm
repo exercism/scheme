5e898355ef008ce172daf38a8afd94929144f692
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2020-06-30 17:25:48 +0800

    Update armstrong-numbers/test.scm
    Use `test-assert` instead of `test-eq`.

--- exercises/armstrong-numbers/test.scm
+++ exercises/armstrong-numbers/test.scm
@@ -4,88 +4,67 @@
 
 (test-begin "armstrong-numbers")
 
-(test-eq "Zero is an Armstrong number"
-  (armstrong-number? 0)
-  #t)
+(test-assert "Zero is an Armstrong number"
+  (armstrong-number? 0))
 
-(test-eq "Single digit numbers are Armstrong numbers"
-  (armstrong-number? 5)
-  #t)
+(test-assert "Single digit numbers are Armstrong numbers"
+  (armstrong-number? 5))
 
-(test-eq "There are no 2 digit Armstrong numbers"
-  (armstrong-number? 10)
-  #f)
+(test-assert "There are no 2 digit Armstrong numbers"
+  (not (armstrong-number? 10)))
 
-(test-eq "Three digit number that is an Armstrong number"
-  (armstrong-number? 153)
-  #t)
+(test-assert "Three digit number that is an Armstrong number"
+  (armstrong-number? 153))
 
-(test-eq "Three digit number that is not an Armstrong number"
-  (armstrong-number? 100)
-  #f)
+(test-assert "Three digit number that is not an Armstrong number"
+  (not (armstrong-number? 100)))
 
-(test-eq "Four digit number that is an Armstrong number"
-  (armstrong-number? 9474)
-  #t)
+(test-assert "Four digit number that is an Armstrong number"
+  (armstrong-number? 9474))
 
-(test-eq "Four digit number that is not an Armstrong number"
-  (armstrong-number? 9475)
-  #f)
+(test-assert "Four digit number that is not an Armstrong number"
+  (not (armstrong-number? 9475)))
 
-(test-eq "Seven digit number that is an Armstrong number"
-  (armstrong-number? 9926315)
-  #t)
+(test-assert "Seven digit number that is an Armstrong number"
+  (armstrong-number? 9926315))
 
-(test-eq "Seven digit number that is not an Armstrong number"
-  (armstrong-number? 9926314)
-  #f)
+(test-assert "Seven digit number that is not an Armstrong number"
+  (not (armstrong-number? 9926314)))
 
-(test-eq "The 25th Armstrong number"
-  (armstrong-number? 24678050)
-  #t)
+(test-assert "The 25th Armstrong number"
+  (armstrong-number? 24678050))
 
-(test-eq "Eight digit number that is not an Armstrong number"
-  (armstrong-number? 30852815)
-  #f)
+(test-assert "Eight digit number that is not an Armstrong number"
+  (not (armstrong-number? 30852815)))
 
-(test-eq "The 28th Armstrong number"
-  (armstrong-number? 146511208)
-  #t)
+(test-assert "The 28th Armstrong number"
+  (armstrong-number? 146511208))
 
-(test-eq "Nine digit number that is not an Armstrong number"
-  (armstrong-number? 927427554)
-  #f)
+(test-assert "Nine digit number that is not an Armstrong number"
+  (not (armstrong-number? 927427554)))
 
-(test-eq "The 32nd Armstrong number"
-  (armstrong-number? 4679307774)
-  #t)
+(test-assert "The 32nd Armstrong number"
+  (armstrong-number? 4679307774))
 
-(test-eq "Ten digit number that is not an Armstrong number"
-  (armstrong-number? 8320172640)
-  #f)
+(test-assert "Ten digit number that is not an Armstrong number"
+  (not (armstrong-number? 8320172640)))
 
-(test-eq "The 34th Armstrong number"
-  (armstrong-number? 32164049651)
-  #t)
+(test-assert "The 34th Armstrong number"
+  (armstrong-number? 32164049651))
 
-(test-eq "Eleven digit number that is not an Armstrong number"
-  (armstrong-number? 13930642218)
-  #f)
+(test-assert "Eleven digit number that is not an Armstrong number"
+  (not (armstrong-number? 13930642218)))
 
-(test-eq "The 66th Armstrong number"
-  (armstrong-number? 4422095118095899619457938)
-  #t)
+(test-assert "The 66th Armstrong number"
+  (armstrong-number? 4422095118095899619457938))
 
-(test-eq "The 77th Armstrong number"
-  (armstrong-number? 1927890457142960697580636236639)
-  #t)
+(test-assert "The 77th Armstrong number"
+  (armstrong-number? 1927890457142960697580636236639))
 
-(test-eq "The 88th Armstrong number"
-  (armstrong-number? 115132219018763992565095597973971522401)
-  #t)
+(test-assert "The 88th Armstrong number"
+  (armstrong-number? 115132219018763992565095597973971522401))
 
-(test-eq "Thirty-nine digit number that is not an Armstrong number"
-  (armstrong-number? 7744959048678381442547644364350528967165)
-  #f)
+(test-assert "Thirty-nine digit number that is not an Armstrong number"
+  (not (armstrong-number? 7744959048678381442547644364350528967165)))
 
 (test-end "armstrong-numbers")
