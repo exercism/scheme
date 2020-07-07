839e58b3b2e204d5a59bead495650f149ba1742e
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2020-07-07 16:14:46 +0800

    Update example.scm

--- exercises/sum-of-multiples/example.scm
+++ exercises/sum-of-multiples/example.scm
@@ -1,11 +1,8 @@
 (use-modules (srfi srfi-1))
 
 (define (sum-of-multiples ints limit)
-  (let ([ints-0 (delete 0 ints)])
-    (if (null? ints-0)
-        0
-        (apply + (filter (lambda (n)
-                           (any (lambda (i)
-                                  (zero? (remainder n i)))
-                                ints-0))
-                         (cdr (iota limit)))))))
+  (apply + (filter (lambda (n)
+                     (any (lambda (i)
+                            (zero? (remainder n i)))
+                          (delete 0 ints)))
+                   (cdr (iota limit)))))
