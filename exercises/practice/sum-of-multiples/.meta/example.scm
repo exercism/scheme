8808dbec024f5e8ef7fd9dfdbcb864a5ff9c2cee
Guy Gastineau <strings.stringsandstrings@gmail.com>
2022-06-09 22:30:49 -0400

    Make several recently re-ported exercises work with both schemes.
    In a recent PR I approved work that reimplemented existing solutions
    that didn't work with our current infrastructure.  Our CI used an
    exercise list kept statically in the Makefile, but it was already out
    of date.  The new CI runs for all practice exercises (with a
    blacklist), so these exercises weren't tested in the CI for the PRs.
    This compound commit contains the fixes needed by these recent
    exercises to pass the new CI for both guile and chez.
    
    Make sum-of-multiples work for both guile and chez.
    
    Make the strain example solution work for both guile and chez.
    
    Make the trinary example solution work for both guile and chez.
    
    Make the triangle example solution work for both guile and chez.

--- exercises/practice/sum-of-multiples/.meta/example.scm
+++ exercises/practice/sum-of-multiples/.meta/example.scm
@@ -1,8 +1,14 @@
-(use-modules (srfi srfi-1))
+(import (rnrs))
+
+(define (delete x xs)
+  (filter (lambda (y) (not (equal? x y))) xs))
+
+(define (any f xs)
+  (fold-left (lambda (r x) (or r (f x))) #f xs))
 
 (define (sum-of-multiples ints limit)
   (apply + (filter (lambda (n)
                      (any (lambda (i)
-                            (zero? (remainder n i)))
-                          (delete 0 ints)))
+                              (zero? (remainder n i)))
+                            (delete 0 ints)))
                    (cdr (iota limit)))))
--- exercises/practice/triangle/.meta/example.scm
+++ exercises/practice/triangle/.meta/example.scm
@@ -1,10 +1,38 @@
-(use-modules (srfi srfi-1))
+(import (rnrs))
+
+(define (any f xs)
+  (fold-left (lambda (r x) (or r (f x))) #f xs))
+
+(define (every f xs)
+  (fold-left (lambda (r x) (and r (f x))) #t xs))
+
+(define (string-any f s)
+  (any f (string->list s)))
+
+(define (string-every f s)
+  (every f (string->list s)))
+
+(define (nub xs)
+  (fold-left (lambda (ys x)
+               (if (any (lambda (y) (equal? x y)) ys) ys
+                   (cons x ys)))
+             '() xs))
+
+(define (impl-safe-sort f xs)
+  (call/cc
+   (lambda (k)
+     (with-exception-handler
+         (lambda (e)
+           ;; Uh-oh, we're using Guile!
+           (k (sort xs f)))
+       (lambda ()
+         (sort f xs))))))
 
 (define (triangle a b c)
-  (let ([xs (sort (list a b c) >)])
+  (let ([xs (impl-safe-sort > (list a b c))])
     (if (>= (car xs) (apply + (cdr xs)))
         (error "Invalid triangle")
-        (case (length (delete-duplicates xs))
+        (case (length (nub xs))
           [(1) 'equilateral]
           [(2) 'isosceles]
           [else 'scalene]))))
